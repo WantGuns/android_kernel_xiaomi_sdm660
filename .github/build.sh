7467659d0368b46b5fcb865a3247416d3b54b115
Gunwant Jain <therealgunwant@gmail.com>
2020-06-25 02:14:02 +0530

    CI: Implement Release Artifacts The workflow now releases verbose artifacts on tag pushed.
    Signed-off-by: Gunwant Jain <therealgunwant@gmail.com>

--- .github/build.sh
+++ .github/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-echo "Cloning dependencies"
 
+DEVICE=lavender
+
+echo "Cloning dependencies"
 #lets keep our directories clean
 mkdir chute
 pushd chute
@@ -9,19 +11,18 @@ git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
 git clone --depth=1 https://github.com/wantguns/AnyKernel3 AnyKernel
 
 echo "Done"
+
 ANYKERNELDIR=$(pwd)/AnyKernel/
 GCC="$(pwd)/aarch64-linux-android-"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-TANGGAL=$(date +"%F-%S")
-START=$(date +"%s")
+
 export CONFIG_PATH=$PWD/arch/arm64/configs/lavender-perf_defconfig
 PATH="${PWD}/clang/bin:$PATH"
 export ARCH=arm64
 export KBUILD_BUILD_HOST=SharkBait
 export KBUILD_BUILD_USER="wantguns"
 
-popd
-#now inside the kernel root
+popd #now inside the kernel root
 
 # Compile plox
 function compile() {
@@ -36,11 +37,9 @@ function compile() {
 # Zipping
 function zipping() {
     pushd chute/AnyKernel || exit 1
-    zip -r9 sharkbait-lavender-${TANGGAL}.zip *
+    zip -r9 sharkbait-$DEVICE.zip *
     popd
 }
 
 compile
 zipping
-END=$(date +"%s")
-DIFF=$(($END - $START))
